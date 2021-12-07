e1b5dbee1bc707fb785ddbd40392285d8fd32006
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-12-07 17:41:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,5 @@ var swiper = new Swiper(".mySwiper", {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev" , 
-      prevEl : ".swiper-button-prev3",
-      nextEl : ".swiper-button-next3",
     },
   });
