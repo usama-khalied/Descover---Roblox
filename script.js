08f7c158e2544813ed787e41efe69780cb8a1b6c
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-12-07 11:43:50 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 8,
+    spaceBetween: 20,
+    slidesPerGroup: 3,
+    loop: true,
+    autoplay:true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
